93885dac9663413f5d7c7fe2b6e034c5692df61d
Aniparna <sopa.sengupta@gmail.com>
2017-04-05 21:48:02 -0500

    sql

---
+++ Customer_details.sql
@@ -0,0 +1,19 @@
+
+INSERT INTO CUSTOMER_DETAILS values (1,1001,1,"Automobile","Allow","NeighbourState");
+INSERT INTO CUSTOMER_DETAILS values (2,1002,2,"Automobile","NotAllow","WithinState");
+INSERT INTO CUSTOMER_DETAILS values (3,1003,3,"Truck","NotAllow","11,12,13");
+INSERT INTO CUSTOMER_DETAILS values (4,1004,1,"Boat","NotConsider","1");
+INSERT INTO CUSTOMER_DETAILS values (5,1005,2,"Automobile","NotAllow","WithinState");
+
+INSERT INTO CUSTOMER_DETAILS values (6,1006,3,"Truck","NotAllow","11,12,13");
+
+INSERT INTO CUSTOMER_DETAILS values (7,1007,1,"Truck","Allow","11,12,13,14");
+
+INSERT INTO CUSTOMER_DETAILS values (8,1008,2,"Automobile","NotAllow","WithinState");
+
+INSERT INTO CUSTOMER_DETAILS values (9,1009,3,"Truck","NotAllow","11,12,13");
+
+INSERT INTO CUSTOMER_DETAILS values (10,1010,1,"Automobile","Allow","NeighbourState");
+
+INSERT INTO CUSTOMER_DETAILS values (11,1011,2,"Anyother","NotConsider","2");
+
